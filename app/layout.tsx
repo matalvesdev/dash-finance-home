94a92a554d41e5da1521b4162062235fc589d4b6
Mateus Alves <mateusalvesbassanelli@gmail.com>
2024-11-07 21:47:26 -0300

    Initial login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
+import localFont from "next/font/local";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 
 export const metadata: Metadata = {
@@ -17,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
         {children}
       </body>
