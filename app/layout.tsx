067960288b0cb237811848154189abde460e33d9
Mateus Alves <mateusalvesbassanelli@gmail.com>
2024-11-08 23:15:20 -0300

    fix: update fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
+import { Mulish } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+const mulish = Mulish({
+  subsets: ['latin-ext']
+})
 
 export const metadata: Metadata = {
-  title: "Dash-Home.ai",
-  description: "Dash Home, organize as finanças da sua casa!",
+  title: "HomeFinance.ai",
+  description: "HomeFinance.ai, organize as finanças da sua casa!",
 };
 
 export default function RootLayout({
@@ -29,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
+        className={`${mulish.className}  dark antialiased`}
       >
         <ClerkProvider appearance={{
           baseTheme: dark,
